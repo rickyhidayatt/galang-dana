fe80dd5441430bf870c9eb36eb9dec30c02ec707
rickyhidayatt <rickyhyouth@gmail.com>
2023-04-25 10:44:24 +0700

    Modifikasi Formatter campaign detail

--- domain/formatter/campaign.go
+++ domain/formatter/campaign.go
@@ -48,20 +48,48 @@ func FormatCampaigns(campaigns []model.Campaign) []CampaignFormater {
 }
 
 type CampaignDetailFormater struct {
-	Id               string   `json:"id"`
-	UserId           string   `json:"user_id"`
-	Name             string   `json:"name"`
-	ShortDescription string   `json:"short_description"`
-	Description      string   `json:"description"`
-	ImageURL         string   `json:"image_url"`
-	GoalAmount       int      `json:"goal_amount"`
-	BackerCount      int      `json:"backer_count"`
-	CurrentAmount    int      `json:"current_amount"`
-	Slug             string   `json:"slug"`
-	Perks            []string `json:"perks"`
+	Id               string                  `json:"id"`
+	UserId           string                  `json:"user_id"`
+	Name             string                  `json:"name"`
+	ShortDescription string                  `json:"short_description"`
+	Description      string                  `json:"description"`
+	ImageURL         string                  `json:"image_url"`
+	GoalAmount       int                     `json:"goal_amount"`
+	BackerCount      int                     `json:"backer_count"`
+	CurrentAmount    int                     `json:"current_amount"`
+	Slug             string                  `json:"slug"`
+	Perks            []string                `json:"perks"`
+	User             campaignUserFormater    `json:"user"`
+	Images           []campaignImageFormater `json:"images"`
+}
+
+type campaignUserFormater struct {
+	Name     string `json:"name"`
+	ImageURL string `json:"image_url"`
+}
+
+type campaignImageFormater struct {
+	ImageUrl  string `json:"image_url"`
+	IsPrimary bool   `json:"is_primary"`
 }
 
 func FormatCampaignDetail(campaign model.Campaign) CampaignDetailFormater {
+	var perks []string
+
+	user := campaignUserFormater{
+		Name:     campaign.Name,
+		ImageURL: campaign.User.AvatarFileName,
+	}
+
+	image := []campaignImageFormater{}
+
+	for _, img := range campaign.Images {
+		images := campaignImageFormater{}
+		images.ImageUrl = img.FileName
+		images.IsPrimary = img.IsPrimary
+
+		image = append(image, images)
+	}
 
 	campaignFormat := CampaignDetailFormater{
 		Id:               campaign.Id,
@@ -69,17 +97,16 @@ func FormatCampaignDetail(campaign model.Campaign) CampaignDetailFormater {
 		Name:             campaign.Name,
 		ShortDescription: campaign.ShortDescription,
 		Description:      campaign.Description,
-		// ImageURL:         "",
-		BackerCount:   campaign.BackerCount,
-		CurrentAmount: campaign.CurrentAmount,
-		Slug:          campaign.Slug,
+		BackerCount:      campaign.BackerCount,
+		CurrentAmount:    campaign.CurrentAmount,
+		Slug:             campaign.Slug,
+		User:             user,
+		Images:           image,
 	}
 
 	if len(campaign.Images) > 0 {
 		campaignFormat.ImageURL = campaign.Images[0].FileName
 	}
-
-	var perks []string
 
 	for _, perk := range strings.Split(campaign.Perks, ",") {
 		perks = append(perks, strings.TrimSpace(perk))
